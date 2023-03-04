d2055d9270066e5b03e724228d3d3e36d2efa0cb
Nancy Clavijo Varela <clavijovarela@gmail.com>
2023-03-03 19:29:42 -0500

    firsh push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+import Search from "./componentes/Search/Search";
+import Card from "./componentes/Card/Card";
+import Pagination from "./componentes/Pagination/Pagination";
+import Filter from "./componentes/Filter/Filter";
+import Navbar from "./componentes/Navbar/Narbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h1 className="text-center mb-3">Characters</h1>
+  <div className="container">
+  <div className="row">
+    Filter component will be placed here
+    <div className="col-lg-8 col-12">
+      <div className="row">
+        Card component will be placed here
+      </div>
     </div>
+  </div>
+  </div>
+</div>
   );
 }
 
